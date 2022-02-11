d42a7d5a6713bb7799e8da5b414ab537a0471888
Kevin Thomas <kevin@mytechnotalent.com>
2022-02-11 05:57:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,11 +12,8 @@ while True:
     pin1.write_digital(1)
     
     display.show(Image.SURPRISED)
-    
     say('The light is on!', speed=SPEED)
-    
     display.show(Image.HAPPY)
-    
     sleep(5)
     
     # Here we write a digital 0 or False to our pin1
@@ -24,9 +21,6 @@ while True:
     pin1.write_digital(0)
     
     display.show(Image.SURPRISED)
-    
     say('The light is off!', speed=SPEED)
-    
     display.show(Image.HAPPY)
-    
     sleep(5)
