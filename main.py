61cf7d077ce547916950f03038441eac00e8d4cc
Kevin Thomas <kevin@mytechnotalent.com>
2022-02-10 20:57:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,14 +10,23 @@ while True:
     # Here we write a digital 1 or True to our pin1
     # as this will turn the LED on
     pin1.write_digital(1)
+    
     display.show(Image.SURPRISED)
+    
     say('The light is on!', speed=SPEED)
+    
     display.show(Image.HAPPY)
+    
     sleep(5)
+    
     # Here we write a digital 0 or False to our pin1
     # as this will turn the LED on
     pin1.write_digital(0)
+    
     display.show(Image.SURPRISED)
+    
     say('The light is off!', speed=SPEED)
+    
     display.show(Image.HAPPY)
+    
     sleep(5)
